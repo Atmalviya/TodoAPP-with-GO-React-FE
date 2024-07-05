065e2668944258a9ba93bd0ddde0db063e577d69
Atul <atulmalviyav2@gmail.com>
2024-07-05 15:48:26 +0530

    Todo List component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+function TodoList({ todos, onUpdate, onDelete }) {
+  console.log('TodoList props:', todos); // Log the todos received
+  return (
+    <ul>
+      {todos.map(todo => (
+        <TodoItem key={todo.id} todo={todo} onUpdate={onUpdate} onDelete={onDelete} />
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
